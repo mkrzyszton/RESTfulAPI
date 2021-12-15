3806e4daa71ae4e96c25879fef7bb90b17208bd5
Michal Krzyszton <michal.krzyszton@microsoft.wsei.edu.pl>
2021-12-15 22:07:48 +0100

    Dodanie klucza

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,7 +1,10 @@
-﻿namespace Products.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Models
 {
     public class Products
     {
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
         public decimal Price{ get; set; }
